451e9fa619907be92b300953190d54f13d4f0086
Caio Rolla <caio.rolla@worthix.com>
2020-08-12 22:13:51 -0300

    :books: Remove old examples

--- example/example.js
+++ example/example.js
@@ -2,11 +2,3 @@
 browserUserData
   .extractData()
   .then(data => document.getElementById('data').innerHTML = JSON.stringify(data))
-
-// browserUserData
-//   .extractConnectedSocialMedias()
-//   .then(socialMedias => document.getElementById('social-medias').innerHTML = JSON.stringify(socialMedias))
-
-
-// browserUserData.extractLocation()
-//   .then(loc => document.getElementById('location').innerHTML = JSON.stringify(loc))
